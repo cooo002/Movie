1c16de309ccab51a51cc83732326786ade34a86c
cooo002 <cooo002@naver.com>
2019-06-02 16:18:31 +0900

    Initial Commit

---
+++ MovieChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieChart
+//
+//  Created by 김재석 on 02/06/2019.
+//  Copyright © 2019 김재석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
